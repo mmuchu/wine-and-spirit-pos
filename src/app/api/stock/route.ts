8a0f8f395f4ba2627e073ed74956b8fd6214d992
mmuchu <mmuchugia@yahoo.com>
2026-03-30 16:16:08 +0300

    fix: manually add await to stock route

--- src/app/api/stock/route.ts
+++ src/app/api/stock/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@/lib/supabase/server'; // Import the server clien
 
 export async function POST(request: Request) {
   // 1. Get the authenticated user
-  const supabase = createClient();
+  const supabase = await createClient();
   const { data: { user }, error: authError } = await supabase.auth.getUser();
 
   if (authError || !user) {
